e1e8f9376b1a254763b5a58955c4bb36c779a0c3
nakajima97 <kituneudon97@gmail.com>
2024-08-24 14:48:58 +0900

    #1 MUIのセットアップ

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,15 @@
 import type { AppProps } from 'next/app';
+import { AppCacheProvider } from '@mui/material-nextjs/v13-pagesRouter';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<AppCacheProvider>
+			<Component {...pageProps} />
+		</AppCacheProvider>
+		);
 }
